2c613e2b40e38c202bbdb10776296778bb274806
yudahadi14 <35730330+yudahadi14@users.noreply.github.com>
2019-07-15 14:27:59 +0700

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/setupTestFramework.js'],
+  transform: {
+    '\\.js$': '<rootDir>/custom-preprocessor.js',
+  },
+  moduleNameMapper: {
+    'netlify-cms-lib-auth': '<rootDir>/packages/netlify-cms-lib-auth/src/index.js',
+    'netlify-cms-lib-util': '<rootDir>/packages/netlify-cms-lib-util/src/index.js',
+    'netlify-cms-ui-default': '<rootDir>/packages/netlify-cms-ui-default/src/index.js',
+  },
+  testURL: 'http://localhost:8080',
+};
